1f7e837773de5dd226301356d360499dd4d85197
Ryan Boldi <ryan.boldi123@gmail.com>
2022-01-01 15:26:43 +0400

    skeleton code

---
+++ src/grid.js
@@ -0,0 +1,15 @@
+class Grid {
+    /**
+     * 
+     * @param {Number} width # of cells in each row of the grid
+     * @param {Number} height # of cells in each col of the grid 
+     */
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+
+    draw(gridSize, topLeft, topRight){
+
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,11 @@
+const WIDTH = 800,
+     HEIGHT = 800;
+
+function setup(){
+    createCanvas(WIDTH, HEIGHT);
+
+}
+
+function draw(){
+    background(220);
+}
