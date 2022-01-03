d12fc02f31c47abf74cb63e5a9f061db222536b4
Ryan Boldi <ryan.boldi123@gmail.com>
2022-01-03 06:43:04 +0400

    drawing arbitrary grids

--- src/grid.js
+++ src/grid.js
@@ -1,15 +1,43 @@
 class Grid {
     /**
      * 
-     * @param {Number} width # of cells in each row of the grid
-     * @param {Number} height # of cells in each col of the grid 
+     * @param {Number} width number of cells in each row of the grid
+     * @param {Number} height number of cells in each col of the grid 
      */
     constructor(width, height){
         this.width = width;
         this.height = height;
+
+        this.cells = new Array(this.width).fill(0).map(() => new Array(this.height).fill(0));
+
+        this.gridSizeWarned = false;
     }
 
-    draw(gridSize, topLeft, topRight){
+    draw(gridSize, topLeftX, topLeftY){
+        //check if gridsize is mismatched, warn the user if so (but only once)
+        if (!this.gridSizeWarned){
+            if (gridSize * this.width != WIDTH || gridSize * this.height != HEIGHT){
+                console.warn("gridsize mismatch with screen size");
+                this.gridSizeWarned = true;
+            }
+        }
 
+
+        //draw the vertical lines
+        for (let i = 0; i < this.width; i++){
+            line(topLeftX + (i * gridSize), 0, topLeftX + (i * gridSize), (gridSize * this.height));
+        }
+
+        //draw the horizontal lines
+        for (let i = 0; i < this.height; i++){
+            line(0, topLeftY + (i * gridSize), (gridSize * this.width), topLeftY + (i * gridSize));
+        }
+
+        //draw double thickness rectangle around the grid
+        strokeWeight(2);
+        noFill();
+        rect(topLeftX, topLeftY, topLeftX + (gridSize * this.width), topLeftY + (gridSize * this.height));
+        strokeWeight(1);
+        
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-const WIDTH = 800,
-     HEIGHT = 800;
+const WIDTH = 700,
+     HEIGHT = 700;
+
+var g;
+
+
 
 function setup(){
     createCanvas(WIDTH, HEIGHT);
-
+    g = new Grid(10, 10);
 }
 
 function draw(){
     background(220);
+    g.draw(70, 0, 0);
 }
